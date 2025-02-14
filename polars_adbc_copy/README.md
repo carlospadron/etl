24ac0a230a0f19575dadb7112b7a2bc648abee2a
carlos.padron <carlos.padron@gigaclear.com>
2025-02-14 09:47:03 +0000

    Update README and main.py to modify Docker run command and database table references

--- polars_adbc_copy/README.md
+++ polars_adbc_copy/README.md
@@ -4,9 +4,14 @@ sudo docker build -t polars_adbc_copy .
 sudo docker images
 cd ..
 sudo docker rm -f polars_adbc_copy || true
-sudo docker run --rm --env-file .env --name polars_adbc_copy polars_adbc_copy
+sudo docker run --rm --env-file .env --name polars_adbc_copy --network host polars_adbc_copy
 ```
 in other terminal
 ```
-./log_memory.sh polars_adbc_copy 5
+./log_memory.sh polars_adbc_copy 1
+```
+Evaluate:
+```
+psql -d target -c "SELECT count(*) FROM os_open_uprn"
+psql -d target -c "TRUNCATE TABLE os_open_uprn"
 ```
--- polars_adbc_copy/main.py
+++ polars_adbc_copy/main.py
@@ -1,6 +1,7 @@
 import polars as pl
 import os
 from io import StringIO
+import psycopg2
 
 #credentials
 TARGET_USER = os.getenv('TARGET_USER')
@@ -13,12 +14,19 @@ ORIGIN_ADDRESS = os.getenv('ORIGIN_ADDRESS')
 ORIGIN_PASS = os.getenv('ORIGIN_PASS')
 ORIGIN_DB = os.getenv('ORIGIN_DB')
 
+conn_target = psycopg2.connect(
+    dbname=TARGET_DB,
+    user=TARGET_USER,
+    password=TARGET_PASS,
+    host=TARGET_ADDRESS
+)
+
 engine_target = f'postgresql://{TARGET_USER}:{TARGET_PASS}@{TARGET_ADDRESS}/{TARGET_DB}'
 
 engine_origin = f'postgresql://{ORIGIN_USER}:{ORIGIN_PASS}@{ORIGIN_ADDRESS}/{ORIGIN_DB}'
 
-table_origin = 'source.table'
-table_target = 'source.table'
+table_origin = 'public.os_open_uprn'
+table_target = 'os_open_uprn'
 query = f"SELECT row_number() over () as fid, * FROM {table_origin}"
 
 #read data from source
@@ -34,13 +42,12 @@ data = pl.read_database_uri(
 #create csv in memory
 print('Creating csv in memory')
 output = StringIO()
-data.write_csv(output, index=False, header=False, sep='|')
+data.write_csv(output, include_header=False, separator='|')
 output.seek(0)
 
 #copy data to destination
 print('Copying data to destination')
-connection = engine_target.raw_connection()
-cursor = connection.cursor()
+cursor = conn_target.cursor()
 cursor.copy_expert(f"COPY {table_target} FROM STDIN DELIMITER '|' CSV", output)
-connection.commit()
+conn_target.commit()
 cursor.close()
