928d311b5ce02e494c975f2ace2976c3243709ef
carlos.padron <carlos.padron@gigaclear.com>
2025-02-14 11:30:51 +0000

    Refactor initial_upload.sh to drop existing table before creating a new one from os_open_uprn_full

--- data/initial_upload.sh
+++ data/initial_upload.sh
@@ -1,3 +1,5 @@
 psql -d postgres -f table_definitions.sql 
 psql -d postgres -c "\copy os_open_uprn_full from 'osopenuprn_202502.csv' with csv header"
-psql -d postgres -c "select * into os_open_uprn from os_open_uprn_full limit 2000000"
+psql -d postgres -c "drop table os_open_uprn"
+psql -d postgres -c "select * into os_open_uprn from os_open_uprn_full"
+#psql -d postgres -c "select * into os_open_uprn from os_open_uprn_full limit 2000000"
